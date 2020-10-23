b9ee9df80590dcc78d191f7eb73b769984887f30
KathyPrim <eeprimakova@stud.etu.ru>
2020-10-23 15:05:34 +0300

    time

--- 3sem/ASD/ASD2/ASD2/ASD2.cpp
+++ 3sem/ASD/ASD2/ASD2/ASD2.cpp
@@ -1,13 +1,29 @@
 ﻿#include<iostream>
 #include"Sort.h"
-#include"Timer.h"
+#include <time.h>
+#include <cstdlib>
 
 using namespace std;
 
-int main() {
-	char b[] = "algorithm";
-	CountingSort(b, 9);
-	for (int i = 0; i < 9; i++) {
-		cout << b[i];
+const int rand_1 = 1103515245, rand_2 = 12345, rand_3 = 65536, rand_max = 1001;
+
+void SortTime(int size, int function_type) { // 1- QS, 0 - BS
+	int* Array = new int[size];
+	int next = (int)clock();
+	for (int i = 0; i < size; i++) {
+		next = ((next * rand_1 + rand_2) / rand_3) % rand_max;
+		Array[i] = next;
 	}
+	clock_t beg = clock(); // time in ms
+	if (function_type) {
+		QuickSort(Array, size);
+	}
+	else {
+		BubbleSort(Array, size);
+	}
+	clock_t end = clock(); // time in ms
+	cout << end - beg << endl;
+}
+
+int main() {
 }
--- 3sem/ASD/ASD2/ASD2/Timer.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void QuickSortTime (int array_size);
-
-void BubbleSortTime(int array_size);
